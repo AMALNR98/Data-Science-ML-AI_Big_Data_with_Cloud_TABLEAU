b1e23e66a5899bbf0159ae4b438705d4e819afe0
Amal <amalnr123@gmail.com>
2023-07-29 11:25:34 +0530

    new files added

--- PYTHON/Python_Basics/Flow controllers/Decision Making/Jumping Statements/jumpint_statements.py
+++ PYTHON/Python_Basics/Flow controllers/Decision Making/Jumping Statements/jumpint_statements.py
@@ -1,12 +1,30 @@
-#Jumpming Statement
+# Jumpming Statement
 
 # break
 # continue
 # pass
 
 # break     : To exit a loop
-
+"""
 for i in range(1,51):
     if i == 25:
         break
     print(i)
+"""
+
+# continue : In this the given condition will skip
+"""
+for i in range(1, 51):
+    if i == 25:
+        continue
+    print(i)
+    
+"""
+
+# pass : Pass do nothing
+"""
+for i in range(1, 51):
+    if i == 25:
+        pass
+    print(i)
+"""
