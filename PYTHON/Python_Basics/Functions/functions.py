93010828fc65f53c0198583e9a345995db1bc4f7
Amal <amalnr123@gmail.com>
2023-07-31 12:46:21 +0530

    function syntax

---
+++ PYTHON/Python_Basics/Functions/functions.py
@@ -0,0 +1,12 @@
+# Functions
+# To reduce length of cod
+# Re-usability
+
+# Syntax:
+
+"""
+def function_Name(arguments):
+    #function_Defnition
+
+#function_Call()  ====> function_Name()
+"""
---
+++ PYTHON/Python_Basics/interview questions/interview_questions.py
@@ -0,0 +1,34 @@
+#1
+"""
+for i in range(-2,-5,-1):
+    print(i)
+# ans : -2, -3, -4
+"""
+#2
+"""
+for i in 'johny':
+    if i == 'o':
+        break
+    print(i, end=' ')
+# ans : j
+"""
+
+#3
+"""
+a = 1
+b = 2
+x = 5
+if a>b:
+    if a < b:
+        x = x + 2
+    else:
+        x = x + 3
+else:
+    x = x + 5
+
+print(x)
+#ans : 10
+"""
+
+#4
+
