2a46d691def6dc071cd83253aa58913f1883abb7
Amal <amalnr123@gmail.com>
2023-08-07 13:24:54 +0530

    list basics : interview questions

---
+++ PYTHON/Python_Basics/python collections/List/List_interview_questions.py
@@ -0,0 +1,34 @@
+# question 1
+lst = [3, 5, 10, 20, 25, 30, 15]
+# predict  result
+print(lst[-0] + lst[-1] + lst[2])
+# ans:
+#       28
+print(lst[-0])
+print(lst[-1])
+print(lst[2])
+
+# question 2
+lst_1 = [4, 10, 6, 20, 15, 25, 12, 8]
+# Form this list create
+# lst_2 = [96, 90, 6, 20, 15, 25, 12, 8]
+
+# ans:
+empty_lst = []
+result = sum(lst_1)
+for i in lst_1:
+    empty_lst.append(result - i)
+print(empty_lst)
+
+# question 2
+lst_2 = [1, 5, 6, 3, 2, 4, 5, 2, 1, 6, 7, 8, 9, 3, 1, 7, 8, 9, 11, 9, 6, 5, 4, 3]
+
+# create list form this
+# [1,6,2,5,1,9,1,11]
+# when ever the series is increase and decrease
+
+
+# ???????????????????????????????????????????????????????????
+
+
+
