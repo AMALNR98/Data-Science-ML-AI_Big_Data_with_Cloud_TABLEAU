8f3edaf2df71db9102e9cabdbb926a7e75140beb
Amal <amalnr123@gmail.com>
2023-08-08 13:41:45 +0530

    dictionary basics : key value pair

---
+++ PYTHON/Python_Basics/python collections/Directory/dictionary.py
@@ -0,0 +1,14 @@
+# Dictionary
+# It's a key value pair
+# 1. How to define
+
+dictionary_empty = {}
+"""
+key value pair  
+id : 101
+first_name : vinay
+last_name : k
+age : 28
+prof : bigdata
+"""
+
