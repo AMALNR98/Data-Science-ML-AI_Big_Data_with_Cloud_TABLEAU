1c96adf4fbf93883334b657ba554951733865fe4
Amal <amalnr123@gmail.com>
2023-08-09 13:50:24 +0530

    File operations

---
+++ PYTHON/Python_Basics/File operations/file_operations.py
@@ -0,0 +1,10 @@
+# File operations
+
+# File ==> used for store data
+
+# 3 types of functions
+#   1. Read : To read a data form a file ===> r
+#   2. Write : To write data into a file     ===> w
+#   3. Append : To update a file         ===> a
+
+# File reference
