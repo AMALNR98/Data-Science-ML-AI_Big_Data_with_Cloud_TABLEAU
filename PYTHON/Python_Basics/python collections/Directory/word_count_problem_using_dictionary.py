324f9275a36d79bd2958d7fe4963983bd844a3c1
Amal <amalnr123@gmail.com>
2023-08-09 13:27:04 +0530

    dictionary basics : completed word count problem

--- PYTHON/Python_Basics/python collections/Directory/word_count_problem_using_dictionary.py
+++ PYTHON/Python_Basics/python collections/Directory/word_count_problem_using_dictionary.py
@@ -12,9 +12,9 @@ print(data)
 # Create an empty dictionary
 
 dictionary_1 = {}
-for i in data:
-    if i not in dictionary_1:
-        dictionary_1[i] = 1
+for element in data:
+    if element not in dictionary_1:
+        dictionary_1[element] = 1
     else:
-        dictionary_1[i] += 1
+        dictionary_1[element] += 1
 print(dictionary_1)
