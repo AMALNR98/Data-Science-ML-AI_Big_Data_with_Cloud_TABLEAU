010ba21904dadb2c5a140659f1f3e94b2d0fb854
Amal <amalnr123@gmail.com>
2023-08-12 09:14:25 +0530

    File operations : workout 1 completed

--- PYTHON/Python_Basics/File operations/workout_1.py
+++ PYTHON/Python_Basics/File operations/workout_1.py
@@ -3,3 +3,52 @@
 # 2. rating above 3.5 name, year, rating, duration
 # 3. year above 2005 and rating above 4, name. year, rating, duration
 # 4. Each year released movies count
+
+movies = open('/home/amal/Downloads/movies_cleaned_pandas.csv', 'r')
+
+
+for i in movies:
+    data = i.rstrip('\n').split(',')
+    # print(data)
+    # years = data[-3]
+    # print(data)
+    print(data[2])
+    year = int(data[2])
+    if year >= 2000:
+        print(data[1:4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# code for find movie count after 2000 released
+# filtered_dictionary = {}
+# year_list_after_2000 = []
+
+#     int_year = int(years)
+#     # print(type(int_year))
+#     year_list_after_2000.append(int_year)
+# # print(year_list_after_2000)
+# # print(len(year_list_after_2000))
+# for year in year_list_after_2000:
+#     # print(year)
+#     # print(type(year))
+#
+#     if year >= 2000:
+#         if year not in filtered_dictionary:
+#             filtered_dictionary[year] = 1
+#         else:
+#             filtered_dictionary[year] += 1
+#
+# for k, v in filtered_dictionary.items():
+#     print(k, ",", v)
