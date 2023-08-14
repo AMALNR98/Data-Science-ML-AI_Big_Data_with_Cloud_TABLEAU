c22520e4754276bc6dbbf7f30e552322b645078d
Amal <amalnr123@gmail.com>
2023-08-14 12:45:51 +0530

    OOPs : class

--- PYTHON/Python_Basics/File operations/temperature_file_workout/workout_1.py
+++ PYTHON/Python_Basics/File operations/temperature_file_workout/workout_1.py
@@ -7,5 +7,15 @@ temp= data[1]
 """
 
 temperature = open('/home/amal/Downloads/temper', 'r')
+dict1 = {}
 for i in temperature:
     data = i.rstrip('\n').split(',')
+    district = data[0]
+    temp = data[1]
+    if district not in dict1:
+        dict1[district] = temp
+    else:
+        old = dict1[district]
+        if temp > old:
+            dict1[district] = temp
+print(dict1)
