4bb005b54f5d3db77a3c5b7db7552e20ce8e137f
Amal <amalnr123@gmail.com>
2023-08-18 12:57:37 +0530

    functional programming :

---
+++ PYTHON/Python_Basics/Functional Programming/Functional_programming.py
@@ -0,0 +1,14 @@
+# Functional programming
+#   A pure function is a function whose output value follows solely from its input values, without any observable side effects.
+#   In functional programming, a program consists entirely of evaluation of pure functions.
+#   Computation proceeds by nested or composed function calls, without changes to state or mutable data.
+
+
+# Types
+#   1. lambda
+#   2. map
+#   3. filter
+#   4. list comprehension
+
+
+
