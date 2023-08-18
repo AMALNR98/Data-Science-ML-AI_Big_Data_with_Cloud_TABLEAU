fbb4ee7376bb4083400441a48f2d134ec073e693
Amal <amalnr123@gmail.com>
2023-08-18 13:55:26 +0530

    functional programming : List comprehension

---
+++ PYTHON/Python_Basics/Functional Programming/list comprehension/list_comprehension.py
@@ -0,0 +1,18 @@
+# list comprehension
+# It offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+# 3 Methods
+
+# Method 1:
+#   Range of elements added into a list
+#       Eg : Empty list 1-100 element add
+
+# Method 2:
+#   Range of elements add into a list based on one condition
+#       Eg : adding odd number to  empty list form another list
+
+# Method 3:
+#   Range of elements add into a list based on more than one conditions
+#
+
+
