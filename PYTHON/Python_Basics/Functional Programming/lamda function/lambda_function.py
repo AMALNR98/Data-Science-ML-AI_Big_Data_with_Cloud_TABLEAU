75b121d74533033a372105c5601c00f412e2b3ce
Amal <amalnr123@gmail.com>
2023-08-18 12:57:52 +0530

    functional programming :

---
+++ PYTHON/Python_Basics/Functional Programming/lamda function/lambda_function.py
@@ -0,0 +1,15 @@
+# lambda function :
+# lambda function is an anonymous function
+
+# syntax:
+# variable_name = lambda arguments:operations
+
+# def add(num1, num2):
+#     sum = num1 + num2
+#     print(sum)
+
+
+# this function will be reduced to :
+
+f = lambda num1, num2: num1 + num2
+print(f(10, 20))
