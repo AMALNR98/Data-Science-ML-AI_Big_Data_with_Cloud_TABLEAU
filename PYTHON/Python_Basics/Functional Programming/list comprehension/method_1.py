9954afb1988c2e6413f3514f5380b43b55c48d69
Amal <amalnr123@gmail.com>
2023-08-19 11:06:28 +0530

    functional programming : List comprehension : method 1\

---
+++ PYTHON/Python_Basics/Functional Programming/list comprehension/method_1.py
@@ -0,0 +1,24 @@
+# lst = []
+# for i in range (1,51):
+#     lst.append(i)
+# print(lst)
+#
+
+# Method 1
+
+# printing 1 to 30 element in list
+lst_1 = [i for i in range(1, 31)]
+print(lst_1)
+
+# printing 1 to 51 elements in list
+lst_2 = [i for i in range(1, 51)]
+print(lst_2)
+
+# printing square of form range 1 to 20
+lst_3 = [(i, i ** 2) for i in range(1, 21)]
+print(lst_3)
+
+# 1 -30 range ===> cube
+lst_4 = [(i, i**3) for i in range(1,30)]
+print(lst_4)
+
