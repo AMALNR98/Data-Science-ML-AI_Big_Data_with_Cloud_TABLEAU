8c46fda2709482e48c5774fc7ffeed5a87c823c6
Amal <amalnr123@gmail.com>
2023-08-19 11:15:32 +0530

    functional programming : List comprehension : method 2

--- PYTHON/Python_Basics/Functional Programming/list comprehension/method_2.py
+++ PYTHON/Python_Basics/Functional Programming/list comprehension/method_2.py
@@ -5,7 +5,11 @@
 lst = [i for i in range(1, 31) if i % 2 == 0]
 print(lst)
 
-# 1 tp 50 range, odd number
+# 1 to 50 range, odd number
 lst_2 = [i for i in range(1, 51) if i % 2 == 1]
 print(lst_2)
+
+# 1 to 50 range, divisible by 5
+lst_3 = [i for i in range(1,51) if i % 5 == 0]
+print(lst_3)
 
