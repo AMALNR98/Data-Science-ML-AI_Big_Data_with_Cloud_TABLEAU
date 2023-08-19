e77efd7f604802ade51990b324075a74167bb571
Amal <amalnr123@gmail.com>
2023-08-19 11:43:50 +0530

    functional programming : List comprehension : method 3

---
+++ PYTHON/Python_Basics/Functional Programming/list comprehension/method_3.py
@@ -0,0 +1,4 @@
+# Method 3
+# [print_1 if condition_1 if else print_2 range]
+# in method 3 there is no "elif" condition
+# [print_1 if condition_1 else print_2 if condition_2 else print_3 if condition_3 ....... else print_n range]
