7d63370e64859e74e5aae7a3c07fe0601fa3fbac
Amal <amalnr123@gmail.com>
2023-08-19 11:52:16 +0530

    functional programming : List comprehension : method 3 : example 1

--- PYTHON/Python_Basics/Functional Programming/list comprehension/method_3.py
+++ PYTHON/Python_Basics/Functional Programming/list comprehension/method_3.py
@@ -1,4 +1,19 @@
 # Method 3
+
 # [print_1 if condition_1 if else print_2 range]
+
 # in method 3 there is no "elif" condition
+
+# for multiple conditions
 # [print_1 if condition_1 else print_2 if condition_2 else print_3 if condition_3 ....... else print_n range]
+
+#
+#
+# Examples
+# -----------------------------
+
+# 1 to 50 even ===> square
+#          odd ===> cube
+
+lst = [i ** 2 if i % 2 == 0 else i ** 3 for i in range(1, 51)]
+print(lst)
