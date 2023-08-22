44c511ed0827e9171707406b492f16744141b94e
Amal <amalnr123@gmail.com>
2023-08-22 13:41:43 +0530

    functional programming : List comprehension : method 3 : example 1

--- PYTHON/Python_Basics/Functional Programming/list comprehension/method_3.py
+++ PYTHON/Python_Basics/Functional Programming/list comprehension/method_3.py
@@ -15,5 +15,5 @@
 # 1 to 50 even ===> square
 #          odd ===> cube
 
-lst = [i ** 2 if i % 2 == 0 else i ** 3 for i in range(1, 51)]
+lst = [(i, i ** 2) if i % 2 == 0 else (i, i ** 3) for i in range(1, 51)]
 print(lst)
