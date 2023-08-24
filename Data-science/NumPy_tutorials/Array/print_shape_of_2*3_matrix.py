b4c74729d7ad39e1761719f2869bd76fa10dc106
Amal <amalnr123@gmail.com>
2023-08-24 13:53:56 +0530

    array using numpy : order and shape

--- Data-science/NumPy_tutorials/Array/print_shape_of_2*3_matrix.py
+++ Data-science/NumPy_tutorials/Array/print_shape_of_2*3_matrix.py
@@ -6,3 +6,10 @@ matrix_1 = np.array([[1, 2, 3], [2, 3, 4], [2, 6, 4], [1, 2, 3, ]])
 print(matrix_1)
 print(matrix_1.ndim)
 print(matrix_1.shape)
+
+# 3d array, (1, 3, 3), (z, x, y)
+
+matrix_2 = np.array([[[1, 2, 3], [1, 2, 3], [2, 3, 5]]])
+print(matrix_2)
+print(matrix_2.ndim)
+print(matrix_2.shape)
