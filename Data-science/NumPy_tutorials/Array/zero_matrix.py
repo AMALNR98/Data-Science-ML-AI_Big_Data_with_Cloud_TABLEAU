72409332eb6458e0f465b1a852930a7e85789789
Amal <amalnr123@gmail.com>
2023-09-04 13:10:22 +0530

    array using numpy : zero matrix

--- Data-science/NumPy_tutorials/Array/zero_matrix.py
+++ Data-science/NumPy_tutorials/Array/zero_matrix.py
@@ -10,7 +10,8 @@
 # zeros function is used to create zero matrix
 import numpy as np
 
-zero_matrix = np.zeros([3,4])
+zero_matrix = np.zeros([3, 4])
+print(zero_matrix.dtype)
 print(zero_matrix)
 
 # To print datatype of a matrix we use 'dtype'
@@ -22,3 +23,27 @@ print(a.dtype)
 b = np.array([4, 5, 6, 7], dtype=float)
 print(b)
 print(b.dtype)
+
+# in zero matrix the data type is float by default
+
+# 2 *2 zero matrix
+two_by_two = np.zeros([2, 2], dtype=int)
+print(two_by_two)
+
+# 3 D ===> 4 * 3
+three_d_two_by_two = np.zeros([1, 4, 3])
+print(three_d_two_by_two)
+print(three_d_two_by_two.ndim)
+print(three_d_two_by_two.shape)
+
+# 1 D ===> 10 zeros
+c = np.zeros([10], dtype=int)
+print(c)
+print(c.ndim)
+print(c.shape)
+
+d = np.zeros([3, 4, 5], dtype=int)
+print(d)
+print(d.ndim)
+print(d.shape)
+print(d.size)  # 60 (4*3*5)
