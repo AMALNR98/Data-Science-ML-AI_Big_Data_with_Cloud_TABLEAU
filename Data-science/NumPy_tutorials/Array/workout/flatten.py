304ca68792ae96747d5845a6eef4e3241aeb1f9f
Amal <amalnr123@gmail.com>
2023-09-05 13:13:53 +0530

    array using numpy : flatten

---
+++ Data-science/NumPy_tutorials/Array/workout/flatten.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+matrix_1 = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 1, 1, 1, 1], [2, 2, 2, 2, 2]])
+print(matrix_1)
+
+# flatten -> for convert multidimensional matrix to one dimension
+one_dimensional_2 = matrix_1.flatten()
+print(one_dimensional_2)
