b2c22f0cb1cf34f5a1600a05b347a6616e82f95b
Amal <amalnr123@gmail.com>
2023-09-06 13:04:32 +0530

    array using numpy : matrices : dot product

---
+++ Data-science/NumPy_tutorials/Array/dot_product.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+matrix_1 = np.array([4, 5, 6, 2, 3, 5])
+matrix_2 = np.array([3, 2, 3, 4, 3, 3])
+print(matrix_1)
+print(matrix_2)
+
+dot_product = np.dot(matrix_1, matrix_2)
+print(dot_product)
