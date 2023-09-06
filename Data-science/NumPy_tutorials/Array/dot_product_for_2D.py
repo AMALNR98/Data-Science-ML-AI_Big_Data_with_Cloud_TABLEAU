bffde95185142e1cdc08e37c9781fef946c134f9
Amal <amalnr123@gmail.com>
2023-09-06 13:06:17 +0530

    array using numpy : matrices : dot product for 2-D matrix

--- Data-science/NumPy_tutorials/Array/dot_product_for_2D.py
+++ Data-science/NumPy_tutorials/Array/dot_product_for_2D.py
@@ -5,3 +5,5 @@ matrix_2 = np.array([[4, 5, 6], [4, 3, 2], [6, 4, 3]])
 
 dot_product = np.dot(matrix_1, matrix_2)
 print(dot_product)
+
+# first matrix's number of row and second matrix's number of columns should be equal
