a7b1331dcf764559408242db69a411fe29ec72ad
Amal <amalnr123@gmail.com>
2023-09-12 13:03:45 +0530

    array using numpy : matrices : filter function : workout

---
+++ Data-science/NumPy_tutorials/Rivision.py
@@ -0,0 +1,40 @@
+#
+
+
+"""
+array
+
+size
+dtype
+zeros
+ones
+full
+identity
+eye complex
+reshape
+flatten
+arange
+linspoace
+add
+sub
+multiply
+divide
+dot
+floor
+ceil
+round
+slicing 1D, 2D
+sort 1D, 2D
+sum
+axis
+max
+min
+argsort
+argmax
+argmin
+ndemurate
+concatenate
+array_split
+where
+filter
+"""
