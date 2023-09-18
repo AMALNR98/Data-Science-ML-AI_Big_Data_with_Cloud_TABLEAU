9e928678f3e60c5e645d3ed0c8bfbe1ad403454e
Amal <amalnr123@gmail.com>
2023-09-18 13:56:18 +0530

    pandas : Nested list : revision

---
+++ Data-science/Pandas Tutorials/rivision.py
@@ -0,0 +1,18 @@
+# select
+# head
+# tail
+# iloc
+# loc
+# sort_values
+# drop_duplicates
+# drop
+
+# loc > sort_values > columns > head/tail
+
+# Evaluating functions
+# sum
+# max
+# min
+# count
+# average
+
