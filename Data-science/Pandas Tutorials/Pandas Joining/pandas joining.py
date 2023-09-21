5a2bcdc390d29eb4225083eef8f19793d457e0a3
Amal <amalnr123@gmail.com>
2023-09-21 12:51:02 +0530

    pandas : joining

--- Data-science/Pandas Tutorials/Pandas Joining/pandas joining.py
+++ Data-science/Pandas Tutorials/Pandas Joining/pandas joining.py
@@ -12,15 +12,15 @@ import numpy as np
 import pandas as pd
 
 nested_dictionary_1 = pd.Series(
-    {'id': [101, 102, 103, 104, 105],
+    {'id': [1, 2, 3, 4, 5],
      'first_name': ['amal', 'john', 'raju', 'alwin', 'robin'],
      'lastname': ['r', 't', 'u', 'r', 'c'],
      'age': [25, 26, 26, 24, 26]})
 print(nested_dictionary_1)
 
 nested_dictionary_2 = pd.Series(
-    {'id': [101, 102, 103, 104, 105],
+    {'id': [3, 4, 5, 6, 7],
      'profession': ['bigdata', 'python', 'st', 'java', 'c'],
      'salary': [10000, 1000, 1500, 2923.2332],
-     'location': ['Thrissur', 'Eranamkulam', 'Ahalakudi', 'Amballur', 'trivandrum']})
+     'location': ['US', 'UK', 'UAE', 'EU', 'JPN']})
 print(nested_dictionary_2)
