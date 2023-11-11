04096a42883411b61cbefb13c844aee5080bf014
Amal <amalnr123@gmail.com>
2023-11-11 05:52:16 +0530

    new files added

---
+++ PYTHON/Coding Questions/nested if pattern printing/Pattern #11: Equilateral Triangle with Stars (Asterisk Symbol).py
@@ -0,0 +1,24 @@
+#           *
+#
+#          * *
+#
+#         * * *
+
+#        * * * *
+
+#       * * * * *
+
+#      * * * * * *
+
+#     * * * * * * *
+
+size = 7
+m = (2*size)-1
+for i in range(0,size):
+    for j in range(0, m):
+        print(end=' ')
+    m = m-1 # decrementing m after each loop
+    for j in range(0, i+1):
+        #Printing full triangle pyramid using stars
+        print("*", end=' ')
+    print(' ')
